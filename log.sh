c6f3872fdd596d16163d4224209911cbd776c8e5
Enamul <mjonyh@gmail.com>
2018-06-11 08:15:47 +0000

    new bug

--- log.sh
+++ log.sh
@@ -2,6 +2,6 @@ tail -fn0 /srv/nfsroot/var/log/slurmd.log |
 	while read -r line; do  
 		if [[ "$line" == *status* ]]; then
 			string=$( echo $line | cut -d " " -f 2,6,7 )
-			echo $string
+			echo "$( date | cut -d " " -f 2,3,4 ) job $string "
 		fi  
 	done
--- slurmctld.sh
+++ slurmctld.sh
@@ -1,6 +1,6 @@
 tail -fn0 /var/log/slurmctld.log |
 	while read -r line; do  
-		if [[ "$line" == *JobID* ]]; then
+		if [[ "$line" == *wrong* ]]; then
 			echo $line  
 		fi  
 	done
