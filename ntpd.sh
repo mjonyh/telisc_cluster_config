a868e0bb282321e67b706a8f313a2b8b5e08ecd9
Enamul <mjonyh@gmail.com>
2018-06-13 07:01:23 +0000

    ntp time set

---
+++ ntpd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+while true
+do 
+
+for i in $(seq $1 $2)
+do
+	#echo "trying on node$i"
+	#sudo ssh node$i systemctl restart ntpd 
+	string=$( sudo ssh node$i ntpdate -u master | cut -d " " -f 10 )
+	#sudo ssh node$i systemctl stop ntpd
+	#sudo ssh node$i ntpdate -q master
+	#sudo ssh node$i systemctl start ntpd
+
+	echo "NTP node$i $( date |cut -d " " -f 2,3,4 ) offset: $string sec"
+
+done
+sleep 2; done
