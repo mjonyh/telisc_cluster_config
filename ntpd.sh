136851c08c4efa205585cf8c5463707855b08f7a
mjonyh <mjonyh@gmail.com>
2018-07-02 09:17:39 +0000

    ntpd updated

--- ntpd.sh
+++ ntpd.sh
@@ -7,7 +7,7 @@ for i in $(seq $1 $2)
 do
 	#echo "trying on node$i"
 	#sudo ssh node$i systemctl restart ntpd 
-	string=$( sudo ssh node$i ntpdate -u master | cut -d " " -f 10 )
+	string=$( sudo ssh node$i ntpdate -u master | cut -d " " -f 11 )
 	#sudo ssh node$i systemctl stop ntpd
 	#sudo ssh node$i ntpdate -q master
 	#sudo ssh node$i systemctl start ntpd
