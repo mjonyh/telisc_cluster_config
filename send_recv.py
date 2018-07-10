cced2d7012dddeaf488486dcd7da675c129bc9a2
mjonyh <mjonyh@gmail.com>
2018-07-10 16:24:03 +0600

    plot added

--- send_recv.py
+++ send_recv.py
@@ -1,6 +1,9 @@
 #!/bin/python3
 
 import sys
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
 
 #print (sys.argv[1])
 
@@ -22,5 +25,16 @@ for line in myFile:
 log_count.pop(0)
 log_count.append(count)
 
-print (log_date, log_count)
-print(len(log_date), len(log_count))
+objects = log_date
+y_pos = np.arange(len(objects))
+performance = log_count
+
+plt.bar(y_pos, performance, align='center', alpha=0.5)
+plt.xticks(y_pos, objects)
+plt.ylabel('Occurance of Send/Recv Errors')
+plt.title('var filesystem in RAM')
+
+plt.show()
+
+#print (log_date, log_count)
+#print(len(log_date), len(log_count))
