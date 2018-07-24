d8fe5fdbd295a159e7953e42768b13c73ff73cfb
mjonyh <mjonyh@gmail.com>
2018-07-24 05:56:18 +0000

    modified

--- job_requeue.sh
+++ job_requeue.sh
@@ -1,12 +1,15 @@
 tail -fn0 /var/log/slurmctld.log |
 	while read -r line; do  
 		if [[ "$line" == *STATUS* ]]; then
+			echo "$( echo $line | cut -d " " -f 1,3,5,7 )"
 			job_status="$(echo $line | cut -d " " -f 7 )"
 			job_id="$( echo $line | cut -d " " -f 3 )"
 			job_id="$( echo $job_id | cut -d "=" -f 2 )"
-			if [[ $job_status != 0 ]] && [[ $job_status != 137 ]]; then
-				sudo scontrol requeue $job_id
-				echo "requeued job $job_id"
+			if [[ $job_status != 0 ]]; then
+			       if [[ $job_status != 137 ]]; then
+				       sudo scontrol requeue $job_id
+       				       echo "requeued job $job_id"
+			       fi
 			fi
 		fi  
 	done
