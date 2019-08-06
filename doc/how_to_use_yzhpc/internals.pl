bc4a2aa8ab6e413b739c2fac9451d88fcb6ece3f
mjonyh <mjonyh@gmail.com>
2019-08-06 08:10:38 +0600

    tutorial of using hpc added

---
+++ doc/how_to_use_yzhpc/internals.pl
@@ -0,0 +1,26 @@
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# Associate internals original text with physical files.
+
+
+$key = q/cha:conclusion/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:introduction/;
+$ref_files{$key} = "$dir".q|node1.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:using_shell_in_yzhpc/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:login/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:useful_linux_command/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ doc/how_to_use_yzhpc/labels.pl
@@ -0,0 +1,33 @@
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# Associate labels original text with physical files.
+
+
+$key = q/cha:conclusion/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:introduction/;
+$external_labels{$key} = "$URL/" . q|node1.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:using_shell_in_yzhpc/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:login/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:useful_linux_command/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# labels from external_latex_labels array.
+
+
+1;
+
